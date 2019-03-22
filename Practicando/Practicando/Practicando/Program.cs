530b11b9342d6ccd6e239a14ba77ccab3861d9b5
GabyVale <48566458+GabyVale@users.noreply.github.com>
2019-03-21 18:13:27 -0600

    Add files via upload

---
+++ Practicando/Practicando/Practicando/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicando
+{
+    public delegate void MiDelegado(int i);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MiClase miClase = new MiClase(); //Creamos un nuevo objeto de clase MiClase
+
+            MiDelegado delegado = new MiDelegado(miClase.MiMetodo); //Instanciamos un delegado
+            delegado(83); //Llamamos al delegado
+        }
+    }
+}
+
+public class MiClase 
+{
+    public void MiMetodo(int i)
+    {
+        Console.WriteLine("El valor pasado como parámetro es: {0}", i);
+        Console.ReadKey();
+    }
+}
